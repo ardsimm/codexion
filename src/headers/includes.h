358b10f84639fb323c50a73415008decbabf400a
smenard <smenard@student.42lyon.fr>
2026-04-23 14:40:51 +0200

    chore(includes.h): Fix header name for pthread.h

--- src/headers/includes.h
+++ src/headers/includes.h
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42lyon.fr >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 13:02:00 by smenard           #+#    #+#             */
-/*   Updated: 2026/04/23 13:27:30 by smenard          ###   ########.fr       */
+/*   Updated: 2026/04/23 14:30:46 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
-# include <pthreads.h>
+# include <pthread.h>
 
 #endif
