c9832eb2778281428eb9ab2f90759f296d43048d
smenard <smenard@student.42lyon.fr>
2026-04-23 13:24:28 +0200

    chore(defines.h): Project structs and enums

--- src/headers/defines.h
+++ src/headers/defines.h
@@ -6,11 +6,82 @@
 /*   By: smenard <smenard@student.42lyon.fr >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 13:07:09 by smenard           #+#    #+#             */
-/*   Updated: 2026/04/23 13:07:21 by smenard          ###   ########.fr       */
+/*   Updated: 2026/04/23 13:23:46 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINES_H
 # define DEFINES_H
+
+# include "headers/includes.h"
+
+typedef enum e_scheduler_mode
+{
+	FIFO,
+	EDF
+}	t_scheduler_mode;
+
+typedef enum e_coder_task
+{
+	COMPILING,
+	DEBUGGING,
+	REFACTORING,
+	NONE
+}	t_coder_task;
+
+typedef struct s_dongle
+{
+	size_t				id;
+	t_coder				*queue;
+	pthread_mutex_t		mutex;
+	t_scheduler_mode	scheduler_mode;
+	size_t				last_use_timestamp;
+}	t_dongle;
+
+typedef struct s_coder
+{
+	size_t				id;
+	t_coder_task		last_task;
+	size_t				last_task_end_timestamp;
+}	t_coder;
+
+/**
+ * Simulation parameters
+ * - {coders_count}: Number of coders in the simulation
+ *
+ * - {dongles}: The dongles of the simulation
+ *
+ * - {coders}: The coders of the simulation
+ *
+ * - {time_to_burnout}: Time until the coders burnout
+ *   (from the beggining of the simulation OR the end of their last compilation)
+ *
+ * - {time_to_compile}: Time required by the coders for the compile task
+ *
+ * - {time_to_debug}: Time required by the coders for the debug task
+ *
+ * - {time_to_refactor}: Time required by the coders for the refactor task
+ *
+ * - {number of compiles}: The number of compilation cycles
+ *   required to finish the simulation
+ *
+ * - {dongle_cooldown}: The timeout before a dongle can be used again
+ *   after being released by a coder
+ *
+ * - {logging_mutex}: Global mutex for thread-safe logging
+ */
+typedef struct s_simulation
+{
+	size_t				coders_count;
+	t_dongle			*dongles;
+	t_coder				*coders;
+	size_t				time_to_burnout;
+	size_t				time_to_compile;
+	size_t				time_to_debug;
+	size_t				time_to_refactor;
+	size_t				number_of_compiles;
+	size_t				dongle_cooldown;
+	pthread_mutex_t		logging_mutex;
+}	t_simulation;
 
 #endif
