20d6c069452868d74bd61fdf4655a1e22f8c68e2
smenard <smenard@student.42lyon.fr>
2026-04-23 13:25:55 +0200

    chore(*): Add string.h and threads.h to project includes

--- src/headers/includes.h
+++ src/headers/includes.h
@@ -6,14 +6,18 @@
 /*   By: smenard <smenard@student.42lyon.fr >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 13:02:00 by smenard           #+#    #+#             */
-/*   Updated: 2026/04/23 13:03:33 by smenard          ###   ########.fr       */
+/*   Updated: 2026/04/23 13:27:30 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDES_H
 # define INCLUDES_H
 
+# include "headers/defines.h"
+
 # include <stdlib.h>
 # include <stdio.h>
+# include <string.h>
+# include <pthreads.h>
 
 #endif
